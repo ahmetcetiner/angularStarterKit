898dadba89557cca521eb196c3afa222525d4e39
Ahmet ÇETİNER <ahmetcetinerrrymuh@gmail.com>
2023-08-08 15:26:03 +0300

    refactor: routing added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientLayoutComponent } from './components';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ClientLayoutComponent,
+    data: { title: ''},
+    children: [
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
