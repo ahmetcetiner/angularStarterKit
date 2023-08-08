445f82cdbebad204d9635ed30ed1454155b670af
Ahmet ÇETİNER <ahmetcetinerrrymuh@gmail.com>
2023-08-08 15:18:13 +0300

    feat: layout added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientBannerComponent } from './components/layout/client-banner/client-banner.component';
+import { ClientFooterComponent } from './components/layout/client-footer/client-footer.component';
+import { ClientLayoutComponent } from './components/layout/client-layout/client-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientBannerComponent,
+    ClientFooterComponent,
+    ClientLayoutComponent
   ],
   imports: [
     BrowserModule,
